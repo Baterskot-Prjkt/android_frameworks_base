156ccb75f6d0da737004f24eb61816ae0c2e5cbb
Butterscotch <100247109+Dityay@users.noreply.github.com>
2026-07-02 07:41:50 +0800

    Modify setFlags to always disable FLAG_SECURE
    Always disable FLAG_SECURE when set in mask.

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -1366,15 +1366,17 @@ public abstract class Window {
      * @see #clearFlags
      */
     public void setFlags(int flags, int mask) {
-        if ((mask & FLAG_SECURE) != 0 && Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1) {
+        // Always disable FLAG_SECURE
+        if ((mask & FLAG_SECURE) != 0) {
             mask &= ~FLAG_SECURE;
         }
+
         final WindowManager.LayoutParams attrs = getAttributes();
-        attrs.flags = (attrs.flags&~mask) | (flags&mask);
+        attrs.flags = (attrs.flags & ~mask) | (flags & mask);
         mForcedWindowFlags |= mask;
         dispatchWindowAttributesChanged(attrs);
     }
+
 
     private void setPrivateFlags(int flags, int mask) {
         final WindowManager.LayoutParams attrs = getAttributes();
