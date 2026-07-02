8187ea79bc83eeb0cf35a14b9ef9d6acf8c25e75
Butterscotch <100247109+Dityay@users.noreply.github.com>
2026-07-03 07:30:21 +0800

    Modify notifyScreenshotListeners to return empty list
    Disable screen capture detections by returning an empty list.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10778,23 +10778,7 @@ public class WindowManagerService extends IWindowManager.Stub
                 "notifyScreenshotListeners()")) {
             throw new SecurityException("Requires STATUS_BAR_SERVICE permission");
         }
-        synchronized (mGlobalLock) {
-            final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
-            if (displayContent == null) {
-                return new ArrayList<>();
-            }
-            ArraySet<ComponentName> notifiedApps = new ArraySet<>();
-            displayContent.forAllActivities(
-                    (ar) -> {
-                        if (!notifiedApps.contains(ar.mActivityComponent) && ar.isVisible()
-                                && ar.isRegisteredForScreenCaptureCallback()) {
-                            ar.reportScreenCaptured();
-                            notifiedApps.add(ar.mActivityComponent);
-                        }
-                    },
-                    true /* traverseTopToBottom */);
-            return List.copyOf(notifiedApps);
-        }
+        return new ArrayList<>(); // Disable screen capture detections by returning empty list
     }
 
     /**
